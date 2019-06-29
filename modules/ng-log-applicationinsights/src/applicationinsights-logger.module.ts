55c977ee7a02c6c1c2867f56500ca736a88f9e33
Moe Myat Zaw <mmzliveid@gmail.com>
2019-06-30 00:41:50 +0630

    Initial commit

---
+++ modules/ng-log-applicationinsights/src/applicationinsights-logger.module.ts
@@ -0,0 +1,47 @@
+/**
+ * @license
+ * Copyright DagonMetric. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found under the LICENSE file in the root directory of this source tree.
+ */
+
+import { ModuleWithProviders, NgModule } from '@angular/core';
+
+import { LOGGER_PROVIDER } from '@dagonmetric/ng-log';
+
+import {
+    APPLICATIONINSIGHTS_LOGGER_OPTIONS,
+    ApplicationInsightsLoggerOptions,
+    ApplicationInsightsLoggerProvider
+} from './applicationinsights-logger-provider';
+
+/**
+ * The `NGMODULE` for providing `LoggerProvider` with `ApplicationInsightsLoggerProvider`.
+ */
+@NgModule({
+    providers: [
+        {
+            provide: LOGGER_PROVIDER,
+            useClass: ApplicationInsightsLoggerProvider,
+            multi: true
+        }
+    ]
+})
+export class ApplicationInsightsLoggerModule {
+    /**
+     * Call this method to provide options for configuring the `ApplicationInsightsLoggerProvider`.
+     * @param options An option object for `ApplicationInsightsLoggerProvider`.
+     */
+    static withOptions(options: ApplicationInsightsLoggerOptions): ModuleWithProviders {
+        return {
+            ngModule: ApplicationInsightsLoggerModule,
+            providers: [
+                {
+                    provide: APPLICATIONINSIGHTS_LOGGER_OPTIONS,
+                    useValue: options
+                }
+            ]
+        };
+    }
+}
---
+++ release-notes/v2.0.0/README.md
@@ -0,0 +1,7 @@
+## Features
+
+WIP
+
+npm package is available on:
+
+[@dagonmetric/ng-log-applicationinsights](https://www.npmjs.com/package/@dagonmetric/ng-log-applicationinsights)
